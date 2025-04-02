fe6c836c0de5e5055bfe8bfbaa85cf75ddf68b55
Mila Jouda <milajouda16@gmail.com>
2025-04-02 21:43:21 +0200

    Removed empty line

--- src/test/java/stepDefintions/stepDefintions.java
+++ src/test/java/stepDefintions/stepDefintions.java
@@ -10,8 +10,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.awt.*;
 import java.time.Duration;
 import java.util.Random;
 
