ce2b3b8120eb08e4b8b74aeabe055549e2761995
Mila Jouda <milajouda16@gmail.com>
2025-04-02 21:41:38 +0200

    G part done

--- src/test/java/stepDefintions/stepDefintions.java
+++ src/test/java/stepDefintions/stepDefintions.java
@@ -1,67 +1,112 @@
 package stepDefintions;
 import io.cucumber.java.After;
-import org.junit.jupiter.api.Test;
-import static java.awt.SystemColor.text;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.RemoteWebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.awt.*;
 import java.time.Duration;
+import java.util.Random;
 
 public class stepDefintions {
-     WebDriver driver;
-     WebDriverWait wait;
-    @Given("I am on the page")
+    WebDriver driver;
+    WebDriverWait wait;
+
+    @Given("I am on the registration page")
     public void iAmOnThePage() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.get("https://membership.basketballengland.co.uk/NewSupporterAccount");
     }
-    @When("I fill in all required fields correctly")
-    public void iFillInAllRequiredFieldsCorrectly() {
 
+    @When("I fill in all required member details fields correctly")
+    public void iFillInAllRequiredFieldsCorrectly() {
+        driver.findElement(By.cssSelector("#dp")).sendKeys("01/01/1990");
         driver.findElement(By.cssSelector("#member_firstname")).sendKeys("Test");
         driver.findElement(By.cssSelector("#member_lastname")).sendKeys("User");
-        driver.findElement(By.cssSelector("#member_emailadress")).sendKeys("testuser1234@example.com");
-        driver.findElement(By.cssSelector("#signup_password")).sendKeys("Password123!");
-        driver.findElement(By.cssSelector("#signup_confirmpassword")).sendKeys("Password123!");
+        Random random = new Random();
+        int randomNumber = 1000 + random.nextInt(9000);
+        driver.findElement(By.id("member_emailaddress")).sendKeys("testuser" + randomNumber + "@example.com");
+        driver.findElement(By.id("member_confirmemailaddress")).sendKeys("testuser" + randomNumber +"@example.com");
+        driver.findElement(By.id("signupunlicenced_password")).sendKeys("Password123!");
+        driver.findElement(By.id("signupunlicenced_confirmpassword")).sendKeys("Password123!");
     }
-    @When("I fill in the fields without a last name")
+
+    @When("I fill in all required member details fields correctly except last name")
     public void iFillInTheFieldsWithoutALastName() {
+        driver.findElement(By.cssSelector("#dp")).sendKeys("01/01/1990");
         driver.findElement(By.cssSelector("#member_firstname")).sendKeys("Test");
-        driver.findElement(By.cssSelector("#member_emailadress")).sendKeys("testuser1234@example.com");
-        driver.findElement(By.cssSelector("#signup_password")).sendKeys("Password123!");
-        driver.findElement(By.cssSelector("#signup_confirmpassword")).sendKeys("Password123!");
+        Random random = new Random();
+        int randomNumber = 1000 + random.nextInt(9000);
+        driver.findElement(By.id("member_emailaddress")).sendKeys("testuser" + randomNumber + "@example.com");
+        driver.findElement(By.id("member_confirmemailaddress")).sendKeys("testuser" + randomNumber +"@example.com");
+        driver.findElement(By.id("signupunlicenced_password")).sendKeys("Password123!");
+        driver.findElement(By.id("signupunlicenced_confirmpassword")).sendKeys("Password123!");
     }
-    @When("I fill in the fields with mismatched passwords")
+
+    @When("I fill in all required member details fields with mismatched passwords")
     public void iFillInTheFieldsWithMismatchedPasswords() {
+        driver.findElement(By.cssSelector("#dp")).sendKeys("01/01/1990");
         driver.findElement(By.cssSelector("#member_firstname")).sendKeys("Test");
         driver.findElement(By.cssSelector("#member_lastname")).sendKeys("User");
-        driver.findElement(By.cssSelector("#member_emailadress")).sendKeys("testuser1234@example.com");
-        driver.findElement(By.cssSelector("#signup_password")).sendKeys("Password123!");
-        driver.findElement(By.cssSelector("#signup_confirmpassword")).sendKeys("Wrong123!");
+        Random random = new Random();
+        int randomNumber = 1000 + random.nextInt(9000);
+        driver.findElement(By.id("member_emailaddress")).sendKeys("testuser" + randomNumber + "@example.com");
+        driver.findElement(By.id("member_confirmemailaddress")).sendKeys("testuser" + randomNumber +"@example.com");
+        driver.findElement(By.id("signupunlicenced_password")).sendKeys("Password123!");
+        driver.findElement(By.id("signupunlicenced_confirmpassword")).sendKeys("Password456!");
     }
+
     @And("I accept the terms and conditions")
     public void iAcceptTheTermsAndConditions() {
-        driver.findElement(By.cssSelector("#consent-confirm")).click();
+        driver.findElement(By.cssSelector("label[for='sign_up_25'] span[class='box']\n")).click();
     }
-    @And("I click  {string}")
+
+    @And("I confirm I am aged over {int}")
+    public void iConfirmIAmAgedOver(int arg0) {
+        driver.findElement(By.cssSelector("label[for='sign_up_26'] span[class='box']\n")).click();
+    }
+
+    @And("I agree on the code of ethics")
+    public void iAgreeOnTheCodeOfEthics() {
+        driver.findElement(By.cssSelector("label[for='fanmembersignup_agreetocodeofethicsandconduct'] span[class='box']\n")).click();
+    }
+
+    @And("I click {string}")
     public void iClick(String button) {
         driver.findElement(By.cssSelector("input[value='" + button + "']")).click();
     }
-    @Then("I should see the message {string}")
-    public void iShouldSeeTheMessage(String message) {
-        String body = driver.findElement(By.tagName("body")).getText().toLowerCase();
-        assertTrue(body.contains(message.toLowerCase()), "Expected message not found: " + message);
-        driver.quit();
+
+    @Then("I should see the message \"THANK YOU FOR CREATING AN ACCOUNT WITH BASKETBALL ENGLAND\"")
+    public void verifySuccessMessage() {
+        WebElement message = driver.findElement(By.cssSelector(".page-content-wrapper h2"));
+        assertEquals("THANK YOU FOR CREATING AN ACCOUNT WITH BASKETBALL ENGLAND", message.getText());
+    }
+
+    @Then("I should see the message \"Last Name is required\"")
+    public void verifyLastNameRequiredMessage() {
+        WebElement message = driver.findElement(By.cssSelector("span[for='member_lastname']"));
+        assertEquals("Last Name is required",message.getText());
+    }
+
+    @Then("I should see the message \"Password did not match\"")
+    public void iShouldSeeTheMessage() {
+        WebElement message = driver.findElement(By.cssSelector("span[for='signupunlicenced_confirmpassword"));
+        assertEquals("Password did not match",message.getText());
+    }
+
+    @Then("I should see the message \"You must confirm that you have read and accepted our Terms and Conditions\"")
+    public void verifyTermsNotAcceptedMessage() {
+        WebElement message = driver.findElement(By.cssSelector("#signup_form span span"));
+        assertEquals("You must confirm that you have read and accepted our Terms and Conditions", message.getText().trim());
     }
 
     @After
@@ -70,4 +115,6 @@ public class stepDefintions {
             driver.quit();
         }
     }
-    }
+}
+
+
--- src/test/resources/feature/registrer.feature
+++ src/test/resources/feature/registrer.feature
@@ -1,28 +1,52 @@
 Feature: user Registration
 
   Scenario: Successful registration
-    Given   I am on the page
-    When    I fill in all required fields correctly
+    Given   I am on the registration page
+    When    I fill in all required member details fields correctly
     And     I accept the terms and conditions
-    And     I click  "Create Supporter Account"
-    Then    I should see the message "THANK YOU FOR CREATING AN ACCOUNT WITH BASKETBALL ENGLAND "
+    And     I confirm I am aged over 18
+    And     I agree on the code of ethics
+    And     I click "CONFIRM AND JOIN"
+    Then    I should see the message "THANK YOU FOR CREATING AN ACCOUNT WITH BASKETBALL ENGLAND"
 
-   Scenario: Missing last name
-    Given   I am on the page
-     When   I fill in the fields without a last name
-     And    I accept the terms and conditions
-     And    I click  "Create Supporter Account"
-     Then   I should see the message "last name is required"
+  Scenario: Missing last name
+    Given   I am on the registration page
+    When    I fill in all required member details fields correctly except last name
+    And     I accept the terms and conditions
+    And     I confirm I am aged over 18
+    And     I agree on the code of ethics
+    And     I click "CONFIRM AND JOIN"
+    Then    I should see the message "Last Name is required"
 
   Scenario: Password do not match
-    Given   I am on the page
-    When    I fill in the fields with mismatched passwords
+    Given   I am on the registration page
+    When    I fill in all required member details fields with mismatched passwords
     And     I accept the terms and conditions
-    And     I click  "Create Supporter Account"
-    Then    I should see the message "Password do not match"
+    And     I confirm I am aged over 18
+    And     I agree on the code of ethics
+    And     I click "CONFIRM AND JOIN"
+    Then    I should see the message "Password did not match"
 
-    Scenario: Terms not accepted
-      Given  I am on the page
-      When   I fill in all required fields correctly
-      And    I click  "Create Supporter Account"
-      Then   I should see the message " You must accept the terms and conditions first"
+  Scenario: Terms and conditions not accepted
+    Given   I am on the registration page
+    When    I fill in all required member details fields correctly
+    And     I confirm I am aged over 18
+    And     I agree on the code of ethics
+    And     I click "CONFIRM AND JOIN"
+    Then    I should see the message "You must confirm that you have read and accepted our Terms and Conditions"
+
+
+  Scenario Outline: Password validation
+    Given   I am on the registration page
+    When    I fill in the fields with "<password>" and "<confirmPassword>"
+    And     I accept the terms and conditions
+    And     I click "CONFIRM AND JOIN"
+    Then    I should see the message "<expectedMessage>"
+
+
+        Examples:
+          | password     | confirmPassword | expectMessage                               |
+          | TestPass123! | WrongPass123!   | "Passwords do not match"                    |
+          | TestPass123! | TestPass123!    | "THANK YOU FOR CREATING AN ACCOUNT"         |
+          | TestPass     | TestPass        | "Passwords must contain special characters" |
+
